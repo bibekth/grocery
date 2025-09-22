510f52d47a85dd9a5740bf947649f7bc74c6f7fa
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-09-22 18:01:05 +0000

    testing webhook

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::group(['namespace' => 'App\Http\Controllers\API', 'as' => 'api.'], function () {
     Route::group(['controller' => 'AuthController'], function () {
